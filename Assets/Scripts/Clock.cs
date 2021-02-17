76d588643e81afb68295e4750f7ee42d090ebd55
abra <55414630+abra-oz@users.noreply.github.com>
2021-02-17 11:48:38 -0600

    Add files via upload

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegree = -30f, minutesToDegree = -6f, secondsToDegree = -6f;
+    
+    [SerializeField]
+    Transform hoursPivot = default, minutesPivot = default, secondsPivot = default;
+
+    void Update () 
+    {
+        //Debug.Log(DateTime.Now.Hour);
+        //uploading
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f , 0f, hoursToDegree * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f , 0f, minutesToDegree * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f , 0f, secondsToDegree * (float)time.TotalSeconds);
+    }
+}
